1aef5e731f042e75262e0d0a34c93f14b18c3b94
big_peng <big_peng@yeah.net>
2019-04-09 13:50:54 +0800

    新增实体类和Main方法

---
+++ src/main/java/com/hzaihua/newapp/entity/User.java
@@ -0,0 +1,6 @@
+package com.hzaihua.newapp.entity;
+
+public class User {
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/hzaihua/newapp/views/Main.java
@@ -0,0 +1,8 @@
+package com.hzaihua.newapp.views;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println("我是add之后添加的代码");
+    }
+}
