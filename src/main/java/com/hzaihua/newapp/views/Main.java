06c78c8e0b7a8a64ef65f277659773b23be1265d
lyq <lyqwhmh2012@163.com>
2019-04-09 15:02:34 +0800

    李永琪：添加代码

--- src/main/java/com/hzaihua/newapp/views/Main.java
+++ src/main/java/com/hzaihua/newapp/views/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("我是add之后添加的代码");
+        System.out.println("这是李永琪提交的代码");
     }
 }
