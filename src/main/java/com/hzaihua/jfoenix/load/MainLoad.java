11927cee6941b822a03be93736dcaae5c2fc6776
big_peng <big_peng@yeah.net>
2019-04-11 08:24:32 +0800

    添加识别屏幕分辨率调节窗口大小

--- src/main/java/com/hzaihua/jfoenix/load/MainLoad.java
+++ src/main/java/com/hzaihua/jfoenix/load/MainLoad.java
@@ -14,6 +14,8 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class MainLoad extends Application {
 
     public MainLoad() {
@@ -29,11 +31,33 @@ public class MainLoad extends Application {
     public void start(Stage primaryStage) throws Exception {
         ViewFlowContext flowContext = new ViewFlowContext();
         flowContext.register("Stage", primaryStage);
+
+        //系统托盘图标
+        /*SystemTray tray = SystemTray.getSystemTray();
+        BufferedImage image = ImageIO.read(MainLoad.class
+                .getResourceAsStream("/views/img/Thunderx1.jpg"));
+        TrayIcon trayIcon = new TrayIcon(image, "自动备份工具");
+        trayIcon.setToolTip("自动备份工具");
+        tray.add(trayIcon);*/
+
+// create flow and flow container, flow container controls view decoration and view exchange
         Flow flow = new Flow(MainController.class);
         DefaultFlowContainer container = new DefaultFlowContainer();
         flow.createHandler(flowContext).start(container);
+
+// JFXDecorator will be applied to primaryStage, and decorated on view which is created by flow container
+        /*JFXDecorator decorator = new JFXDecorator(primaryStage, container.getView(),
+                false, true, true);*/
         CustomJFXDecorator decorator = new CustomJFXDecorator(primaryStage,container.getView(), false, true, true);
-        Scene scene = new Scene(decorator, 1250, 600);
+// init scene with a decorator
+
+        //增加通过屏幕分辨率设置窗口的大小
+        GraphicsEnvironment g = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice sd = g.getDefaultScreenDevice();
+        DisplayMode mode = sd.getDisplayMode();
+        System.out.println("当前系统分辨率"+mode.getWidth()+"*"+mode.getHeight());
+
+        Scene scene = new Scene(decorator, mode.getWidth()*0.65, mode.getHeight()*0.75);
         primaryStage.getIcons().add(new Image("/views/img/Thunderx.jpg"));
         final ObservableList<String> stylesheets = scene.getStylesheets();
         stylesheets.add(MainController.class.getResource("/views/css/main.css").toExternalForm());
