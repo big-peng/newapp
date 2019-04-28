2c8244aba99f4b927600dd63e39f3460f932e404
lyq <lyqwhmh2012@163.com>
2019-04-28 10:40:59 +0800

    编辑设备

--- src/main/java/com/hzaihua/jfoenix/controller/measure/AddFixedMeasureController.java
+++ src/main/java/com/hzaihua/jfoenix/controller/measure/AddFixedMeasureController.java
@@ -2,68 +2,143 @@ package com.hzaihua.jfoenix.controller.measure;
 
 
 import com.hzaihua.jfoenix.entity.InfoMeasure;
+import com.hzaihua.jfoenix.entity.InfoNoiseDevice;
+import com.hzaihua.jfoenix.entity.InfoUser;
 import com.hzaihua.jfoenix.load.device.AddDeviceBeforeLoad;
+import com.hzaihua.jfoenix.load.device.EditDeviceAfterLoad;
+import com.hzaihua.jfoenix.load.noiseDevice.EditNoiseDeviceLoad;
 import com.hzaihua.jfoenix.service.InfoMeasureService;
+import com.hzaihua.jfoenix.service.InfoNoiseService;
 import com.hzaihua.jfoenix.util.BeanFactoryUtil;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.*;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import io.datafx.controller.ViewController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @ViewController(value = "/views/fxml/measure/AddFixedMeasure.fxml")
 public class AddFixedMeasureController {
-    @FXML
-    private JFXButton addFixedDevice;
+    @FXML private JFXButton addFixedDevice; //添加设备按钮
     //添加测点输入项
-    @FXML
-    private Label MeasureType;
-    @FXML
-    private TextField MeasureCode;
-    @FXML
-    private TextField MeasureName;
-    @FXML
-    private TextField MeasureUserName;
-    @FXML
-    private TextField MeasureAddress;
-    @FXML
-    private TextField Longitude;
-    @FXML
-    private TextField Latitude;
-    @FXML
-    private TextField Remark;
-    @FXML
-    private JFXButton fileChoose;
-    @FXML
-    private JFXCheckBox autoConnect;
+    @FXML private Label MeasureType;
+    @FXML private TextField MeasureCode;
+    @FXML private TextField MeasureName;
+    @FXML private TextField MeasureUserName;
+    @FXML private TextField MeasureAddress;
+    @FXML private TextField Longitude;
+    @FXML private TextField Latitude;
+    @FXML private TextField Remark;
+    @FXML private JFXButton fileChoose;
+    @FXML private JFXCheckBox autoConnect;
+    @FXML private ImageView MeasureHead;//图片信息
 
-    @FXML
-    //图片信息
-    private ImageView MeasureHead;
-    @FXML
-    //提示信息
-    private Text actiontarget;
+    //列表信息
+    @FXML private TableView<InfoNoiseDevice> infoNoiseTreeView;
+    @FXML private TableColumn noiseCode;
+    @FXML private TableColumn noiseType;
+    @FXML private TableColumn linkPort;
+    public static ObservableList<InfoNoiseDevice> noiseList = FXCollections.observableArrayList();
 
-    @FXML
-    //提交添加测点按钮
-    private JFXButton commitFixedMeasure;
+    @FXML private HBox editMeasure;
+    @FXML private Text actiontarget;//提示信息
+    @FXML private JFXButton editFixedDevice;//编辑设备按钮
+    @FXML private JFXButton deleteDevice;//删除设备按钮
+    @FXML private JFXButton buforeManager;//前端管理按钮
+    @FXML private JFXButton commitFixedMeasure;//提交添加测点按钮
+
+    @FXML private AnchorPane root;//删除成功后的提示出现地
+    @FXML private JFXDialog dialog;
+    public static InfoNoiseDevice infoNoiseDevice;//需药删除时传入的对象
 
     String path = null;
     InfoMeasureService infoMeasureService = BeanFactoryUtil.getApplicationContext().getBean(InfoMeasureService.class);
+    InfoNoiseService infoNoiseService = BeanFactoryUtil.getApplicationContext().getBean(InfoNoiseService.class);
 
     @PostConstruct
     public void init(){
+        //添加设备按钮
         addFixedDevice.setOnAction(event -> {
             AddDeviceBeforeLoad addDeviceBeforeLoad = new AddDeviceBeforeLoad();
+        });
+
+        //建立删除按钮点击事件
+        JFXSnackbar snackbar = new JFXSnackbar(root);
+        snackbar.setPrefWidth(300);
+        root.getChildren().remove(dialog);
+        //删除设备按钮操作
+        deleteDevice.setOnAction(event -> {
+            JFXAlert alert = new JFXAlert((Stage) deleteDevice.getScene().getWindow());
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setOverlayClose(false);
+            JFXDialogLayout layout = new JFXDialogLayout();
+            layout.setHeading(new Label("提示！"));
+            layout.setBody(new Label("你确定要删除"+infoNoiseDevice.getDeviceCode()+"这条设备信息吗？\n" +
+                    "删除后将无法恢复"));
+            JFXButton trueButton = new JFXButton("确认");
+            JFXButton closeButton = new JFXButton("取消");
+            closeButton.getStyleClass().add("dialog-accept");
+            trueButton.getStyleClass().add("dialog-accept");
+            trueButton.setStyle("-fx-text-fill: #D34336;-fx-background-color: white");
+            closeButton.setOnAction(event1 -> alert.hideWithAnimation());
+            trueButton.setOnAction(event2 -> {
+                noiseList.remove(infoNoiseDevice);
+                //删除成功后的提示，可以根据返回值判断是否删除成功，并弹出对应信息
+                infoNoiseTreeView.setItems(noiseList);
+                snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(
+                        new JFXSnackbarLayout("删除成功", "",null/*event1 -> snackbar.close()*/),
+                        Duration.millis(2000), null));
+                alert.hideWithAnimation();
+                if(infoNoiseDevice == null){
+                    editMeasure.setDisable(true);
+                }
+            });
+            List<JFXButton> buttons = new ArrayList<JFXButton>();
+            buttons.add(trueButton);
+            buttons.add(closeButton);
+            layout.setActions(buttons);
+            alert.setContent(layout);
+            alert.show();
+        });
+
+        //编辑按钮操作
+        editFixedDevice.setOnAction(event -> {
+            EditDeviceAfterLoad editDeviceAfterLoad = new EditDeviceAfterLoad();
+        });
+
+
+
+        //判断设备编号是否已存在
+        MeasureCode.focusedProperty().addListener((ob,old,now)->{
+            if(!now){
+                InfoMeasure infoMeasure = infoMeasureService.queryByMeasureCode(MeasureCode.getText());
+                if(infoMeasure == null){
+                    actiontarget.setText("用户名不存在，可以创建");
+                }else{
+                    actiontarget.setText("用户名存在");
+                }
+            }
         });
 
         //图片选择按钮
@@ -83,10 +158,11 @@ public class AddFixedMeasureController {
                 MeasureHead.setImage(new Image("file:" + path));
             }
         });
-
+        noiseDeviceList();
         InfoMeasure infoMeasure = new InfoMeasure();
         //提交操作
         commitFixedMeasure.setOnAction(event -> {
+            Stage stage = (Stage)commitFixedMeasure.getScene().getWindow();
             String measureType = MeasureType.getText();
             String measureCode = MeasureCode.getText();
             String measureName = MeasureName.getText();
@@ -124,8 +200,33 @@ public class AddFixedMeasureController {
                     infoMeasure.setAutoConnect(0);
                 }
                 infoMeasure.setMeasureHead("file:"+path);
+                StringBuffer buffer = new StringBuffer();
+                for (InfoNoiseDevice infoNoiseDevice : noiseList) {
+                    buffer.append(infoNoiseDevice.getStateType()).append(",").append(infoNoiseDevice.getDeviceCode()).append(";");
+                    infoNoiseDevice.setMeasureCode(measureCode);
+                    infoNoiseService.saveInfoNoiseDevice(infoNoiseDevice);
+                }
+                infoMeasure.setDeviceTypeAndIDs(buffer.toString());
                 infoMeasureService.saveMeasure(infoMeasure);
             }
+            stage.close();
         });
+    }
+    private void noiseDeviceList(){
+        noiseCode.setCellValueFactory(new PropertyValueFactory<>("deviceCode"));
+        noiseType.setCellValueFactory(new PropertyValueFactory<>("deviceType"));
+        linkPort.setCellValueFactory(new PropertyValueFactory<>("linkPort"));
+        infoNoiseTreeView.setItems(noiseList);
+
+        //表格中的点击事件
+        infoNoiseTreeView.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<InfoNoiseDevice>() {
+                    @Override
+                    public void changed(ObservableValue<? extends InfoNoiseDevice> observable, InfoNoiseDevice oldValue, InfoNoiseDevice newValue) {
+                        infoNoiseDevice = newValue;
+                        editMeasure.setDisable(false);
+                    }
+                }
+        );
     }
 }
--- src/main/java/com/hzaihua/jfoenix/entity/StateNoise.java
+++ src/main/java/com/hzaihua/jfoenix/entity/StateNoise.java
@@ -1,7 +1,23 @@
 package com.hzaihua.jfoenix.entity;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class StateNoise {
+    private StringProperty deviceCode;//设备编号
     private int linkState;//设备的连接状态
+
+    public String getDeviceCode() {
+        return deviceCode.get();
+    }
+
+    public StringProperty deviceCodeProperty() {
+        return deviceCode;
+    }
+
+    public void setDeviceCode(String deviceCode) {
+        this.deviceCode = new SimpleStringProperty(deviceCode);
+    }
 
     public int getLinkState() {
         return linkState;
